9ab888cbb33dff87b892d96d28a77b9b8c51978f
jaimecastillo1972 <jaime.castillo.mt@correo.usa.edu.co>
2021-11-26 20:10:27 -0500

    jue

--- src/main/java/com/usa/divinacomedia/app/model/User.java
+++ src/main/java/com/usa/divinacomedia/app/model/User.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
--- src/main/java/com/usa/divinacomedia/app/services/HairProductService.java
+++ src/main/java/com/usa/divinacomedia/app/services/HairProductService.java
@@ -2,7 +2,6 @@ package com.usa.divinacomedia.app.services;
 
 import com.usa.divinacomedia.app.model.HairProduct;
 import com.usa.divinacomedia.app.repositories.HairProductRepository;
-import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
